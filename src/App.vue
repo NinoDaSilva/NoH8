e89ea85789311660185237549d10d850a0fc840c
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-21 13:10:41 +0100

    add Footer & Header layout to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
+import MyHeader from './components/MyHeader.vue';
+import MyFooter from './components/MyFooter.vue';
 </script>
 
 <template>
+  <MyHeader />
+
   <Suspense>
     <RouterView />
   </Suspense>
+
+  <MyFooter />
 </template>
