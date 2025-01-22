9772783619deadd8f63ed101961cf96250784e4f
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-22 11:06:33 +0100

    update homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
 import MyPopup from '@/components/MyPopup.vue';
+import { ref } from 'vue';
+
+const isHomeVisible = ref(false);
+
+setTimeout(() => {
+    isHomeVisible.value = true;
+}, 23000);
 </script>
 
 <template>
-    <main>
-        <MyPopup />
+    <MyPopup v-if="!isHomeVisible" class="opacity-100 transition-opacity duration-500"
+        :class="{ 'opacity-0': isHomeVisible }" />
+    <main v-if="isHomeVisible" class="p-[5%] pt-20 sm:pt-24 opacity-0 transition-opacity duration-500"
+        :class="{ 'opacity-100': isHomeVisible }">
     </main>
 </template>
