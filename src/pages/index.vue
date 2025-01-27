f8f380aabd76a2577493b1099e52e28d60f69d7f
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-27 10:36:16 +0100

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,18 +4,15 @@ import { ref } from 'vue';
 
 const isHomeVisible = ref(true);
 
-// setTimeout(() => {
-//     isHomeVisible.value = true;
-// }, 23000);
+setTimeout(() => {
+    isHomeVisible.value = true;
+}, 23000);
 </script>
 
 <template>
-    <!-- <MyPopup v-if="!isHomeVisible" class="opacity-100 transition-opacity duration-500"
-        :class="{ 'opacity-0': isHomeVisible }" /> -->
+    <MyPopup v-if="!isHomeVisible" class="opacity-100 transition-opacity duration-500"
+        :class="{ 'opacity-0': isHomeVisible }" />
     <main v-if="isHomeVisible" class="p-[5%] pt-20 sm:pt-24 opacity-0 transition-opacity duration-500"
         :class="{ 'opacity-100': isHomeVisible }">
-        <video autoplay muted loop class="absolute inset-0 h-[45vh] md:h-[75vh] w-full object-cover blur-sm">
-            <source src="/videos/baner1.mp4" type="video/mp4">
-        </video>
     </main>
 </template>
