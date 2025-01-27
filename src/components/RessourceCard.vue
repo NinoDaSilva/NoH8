ee6e76b4203f2927bf519bb29eda50906fdeba56
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-27 17:05:39 +0100

    create component : ressources card

---
+++ src/components/RessourceCard.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import RessourceCardBtn from '@/components/RessourceCardBtn.vue';
+defineProps<{
+    titleOne: string,
+    titleTwo: string,
+    imgUrl: string,
+    textColor?: string,
+    iconColor?: string,
+    cardLink: string,
+    bgCardColor?: string,
+}>()
+</script>
+
+<template>
+    <div>
+        <div>
+            <h3>
+                <span>{{ titleOne }}</span>
+                <span>{{ titleTwo }}</span>
+            </h3>
+            <RessourceCardBtn :cardLink="cardLink" :textColor="textColor" :iconColor="iconColor"
+                :bgCardColor="bgCardColor" />
+        </div>
+        <img :src="imgUrl" alt="illustration">
+    </div>
+</template>
