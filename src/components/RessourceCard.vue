c95cb95a145d647d0da06d4f6b67fb92ae39823d
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-28 14:44:01 +0100

    add responsive design to card

--- src/components/RessourceCard.vue
+++ src/components/RessourceCard.vue
@@ -7,20 +7,44 @@ defineProps<{
     textColor?: string,
     iconColor?: string,
     cardLink: string,
+    bgIconColor?: string,
     bgCardColor?: string,
+    highlightTitle?: string,
 }>()
 </script>
 
 <template>
-    <div>
-        <div>
-            <h3>
-                <span>{{ titleOne }}</span>
-                <span>{{ titleTwo }}</span>
+    <RouterLink :to="cardLink" class="w-full max-sm:max-w-[500px] max-w-full">
+        <div class="relative border border-noir rounded-3xl p-6 md:p-8 lg:p-10 sm:mx-0 min-h-full flex flex-col justify-between max-w-[650px] lg:h-[400px] ressource-card group"
+            :class="bgCardColor === 'vert' ? 'bg-vert' : (bgCardColor === 'gris' ? 'bg-gris' : 'bg-noir')">
+
+            <h3 class="text-sm md:text-base lg:text-lg font-medium leading-5 md:leading-6 lg:leading-8 mb-4">
+                <div>
+                    <span class="rounded-md px-1" :class="highlightTitle === 'vert' ? 'bg-vert' : 'bg-blanc'">
+                        {{ titleOne }}
+                    </span>
+                </div>
+                <div>
+                    <span class="rounded-md px-1" :class="highlightTitle === 'vert' ? 'bg-vert' : 'bg-blanc'">
+                        {{ titleTwo }}
+                    </span>
+                </div>
             </h3>
+            <img :src="imgUrl" alt="illustration"
+                class="w-full ml-auto max-w-[150px] md:max-w-[200px] lg:max-w-[250px] transition-transform duration-300 group-hover:scale-105">
             <RessourceCardBtn :cardLink="cardLink" :textColor="textColor" :iconColor="iconColor"
-                :bgCardColor="bgCardColor" />
+                :bgIconColor="bgIconColor" class="absolute bottom-5 lg:bottom-8" />
         </div>
-        <img :src="imgUrl" alt="illustration">
-    </div>
+    </RouterLink>
 </template>
+
+<style>
+.ressource-card {
+    transition: all ease 300ms;
+    box-shadow: 0 2px 0;
+}
+
+.ressource-card:hover {
+    box-shadow: 1px 4px 0;
+}
+</style>
--- src/components/RessourceCardBtn.vue
+++ src/components/RessourceCardBtn.vue
@@ -12,9 +12,9 @@ defineProps<{
     <RouterLink :to="cardLink">
         <div class="cursor-pointer inline-flex items-center gap-3 group">
             <IconCircleArrow
-                class="w-[28px] h-[28px] sm:w-[38px] sm:h-[38px] transition-all ease-out duration-200 group-hover:rotate-[0.5rad]"
+                class="w-[28px] h-[28px] md:w-[38px] md:h-[38px] transition-all ease-out duration-200 group-hover:rotate-[0.5rad]"
                 :arrowColor="iconColor" :bgColor="bgIconColor" />
-            <p class="max-sm:hidden sm:text-base transition-all ease-out group-hover:font-medium"
+            <p class="hidden lg:inline text-base transition-all ease-out group-hover:font-medium"
                 :class="textColor === 'noir' ? 'text-noir' : 'text-blanc'">
                 En savoir plus
             </p>
