1b5b307317a5464b953dcc8a3b3db4c9eb46fea3
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-28 19:04:31 +0100

    update responsive style

--- src/pages/help.vue
+++ src/pages/help.vue
@@ -4,12 +4,11 @@ import MyHero from '@/components/MyHero.vue';
 
 <template>
     <main class="p-[5%] pt-20 sm:pt-24 max-w-[1920px] mx-auto">
-        <MyHero titleOne="Besoin d'aide ?" titleTwo="Une réalité qui dépasse l'écran"
-            imgUrl="/img/HomebannerIllustration.png"
-            description="Que vous soyez victime, témoin, parent ou professionnel, cette page regroupe des ressources essentielles pour vous soutenir." />
+        <MyHero titleOne="Besoin d'aide ?" titleTwo="Services et contacts utiles" imgUrl="/img/illu-help.svg"
+            description="Que vous soyez victime, témoin, parent ou professionnel, cette page regroupe des contacts essentiels pour vous soutenir." />
         <section class="mt-5 lg:mt-10 xl:px-[5%]">
-            <h2 class="text-lg font-semibold">Vous êtes victime ? Voici les étapes à suivre</h2>
-            <ul class="mt-2 list-disc list-inside">
+            <h2 class="text-lg lg:text-xl font-semibold">Vous êtes victime ? Voici les étapes à suivre</h2>
+            <ul class="mt-2 list-disc text-xs lg:text-sm list-inside">
                 <li><strong>Conservez des preuves :</strong> capturez les messages, photos ou vidéos incriminants.</li>
                 <li><strong>Sécurisez vos comptes :</strong> modifiez vos mots de passe et activez la double
                     authentification.</li>
@@ -29,46 +28,59 @@ import MyHero from '@/components/MyHero.vue';
             </p>
             <p class="mt-2 font-medium">Contacter un service d’aide</p>
         </section>
-        <section class="mt-5 lg:mt-10 p-8 rounded-2xl bg-vert max-w-[1100px] mx-auto">
+        <section class="mt-5 lg:mt-10 p-5 md:p-8 rounded-2xl bg-vert shadow-lg max-w-[1100px] mx-auto">
             <h2 class="text-lg md:text-2xl font-semibold leading-tight">Contacts et services à votre disposition</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-                <RouterLink to="">
-                    <div class="p-4 bg-blanc rounded-lg shadow">
-                        <h3 class="text-lg font-bold">Net Écoute</h3>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-3 sm:gap-6 mt-6">
+                <a href="https://e-enfance.org/notre-temps-cyberviolences-le-numero-vert-net-ecoute-devient-le-3018-plus-simple-et-efficace/"
+                    target="_blank">
+                    <div class="p-4 bg-blanc min-h-full rounded-lg shadow contact-card">
+                        <h3 class="text-base sm:text font-bold">Net Écoute</h3>
                         <p class="mt-2">
                             Service d’écoute et d’accompagnement pour les jeunes victimes. Disponible au 3018 ou via
                             leur
                             site web.
                         </p>
                     </div>
-                </RouterLink>
-                <RouterLink to="">
-                    <div class="p-4 bg-blanc rounded-lg shadow">
-                        <h3 class="text-lg font-bold">Pharos</h3>
+                </a>
+                <a href="https://www.internet-signalement.gouv.fr/PharosS1/" target="_blank">
+                    <div class="p-4 bg-blanc min-h-full rounded-lg shadow contact-card">
+                        <h3 class="text-base sm:text-lg font-bold">Pharos</h3>
                         <p class="mt-2">
                             Plateforme gouvernementale pour signaler des contenus illicites en ligne.
                         </p>
                     </div>
-                </RouterLink>
-                <RouterLink to="">
-                    <div class="p-4 bg-blanc rounded-lg shadow">
-                        <h3 class="text-lg font-bold">e-Enfance</h3>
+                </a>
+                <a href="https://e-enfance.org/besoin-daide/" target="_blank">
+                    <div class="p-4 bg-blanc min-h-full rounded-lg shadow contact-card">
+                        <h3 class="text-base sm:text-lg font-bold">e-Enfance</h3>
                         <p class="mt-2">
                             Association spécialisée dans la protection des enfants en ligne. Propose des conseils et
                             ressources.
                         </p>
                     </div>
-                </RouterLink>
-                <RouterLink to="">
-                    <div class="p-4 bg-blanc rounded-lg shadow">
-                        <h3 class="text-lg font-bold">Police/Gendarmerie</h3>
+                </a>
+                <a href="https://www.internet-signalement.gouv.fr/PharosS1" target="_blank">
+                    <div class="p-4 bg-blanc min-h-full rounded-lg shadow contact-card">
+                        <h3 class="text-base sm:text-lg font-bold">Police/Gendarmerie</h3>
                         <p class="mt-2">
                             Déposer une plainte auprès des forces de l’ordre pour signaler des actes graves de
                             cyberharcèlement.
                         </p>
                     </div>
-                </RouterLink>
+                </a>
             </div>
         </section>
     </main>
 </template>
+
+<style>
+.contact-card {
+    transition: all ease 300ms;
+    box-shadow: 0 2px 0;
+}
+
+.contact-card:hover {
+    box-shadow: 2px 4px 0;
+    transform: scale(1.01);
+}
+</style>
