234468f0fdc3f8c7192253cb9db871e59b3c3ade
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-29 10:50:36 +0100

    create backeng.ts & import pocketbase

---
+++ src/backend.ts
@@ -0,0 +1,11 @@
+// Import Pocketbase
+import PocketBase from 'pocketbase';
+// PocketBase vps connection
+var pocketbase_ip=''
+if(import.meta.env.MODE === 'production')
+  pocketbase_ip='https://nohate.nino-da-silva.fr'
+else
+  pocketbase_ip='http://127.0.0.1:8090'
+
+export const pb = new PocketBase(pocketbase_ip);
+//
