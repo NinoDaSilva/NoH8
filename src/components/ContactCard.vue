fceaef320266fc7e4845b6e5b734610c97c1d586
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-29 11:21:17 +0100

    create component : ContactCard

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { ContactResponse } from '@/pocketbase-types';
+defineProps<ContactResponse>();
+</script>
+
+<template>
+    <div>
+        <a :href="link" target="_blank">
+            <div class="p-4 bg-blanc min-h-full rounded-lg shadow contact-card">
+                <h3 class="text-base sm:text font-bold">{{ name }}</h3>
+                <p class="mt-2">{{ description }}</p>
+            </div>
+        </a>
+    </div>
+</template>
