602f5e239170802a0385be0d203bbe223d5ef0f4
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-01-29 22:06:34 +0100

    add credit page

---
+++ src/pages/credits.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <section class="p-[5%] pt-20 sm:pt-24 max-w-[1920px] mx-auto">
+        <h1 class="text-xl md:text-2xl font-semibold my-2">Crédits</h1>
+        <div class="max-w-[800px] md:text-sm">
+            <p class="">
+                Ce modèle a été adapté du design original de
+                <a href="https://www.figma.com/@olgaaverchenko" target="_blank" class="underline">@olgaaverchenko</a>
+                de la communauté Figma. Le modèle comprend des images de
+                <a href="https://www.freepik.com/free-vector/pharmaceutical-marketing-abstract-concept-vector-illustration-pharmaceutical-digital-agency-medicine-marketing-strategy-drugs-advertising-medical-equipment-market-promotion-abstract-metaphor_24070982.htm"
+                    target="_blank" class="underline">vectorjuice sur Freepik</a>.
+            </p>
+            <p class="mt-2">Quelques modifications ont été apportées au design pour ce site, notamment le
+                remplacement
+                d'images et de contenus textuels.</p>
+        </div>
+    </section>
+</template>
