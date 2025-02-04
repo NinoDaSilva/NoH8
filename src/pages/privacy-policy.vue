2ef5ceb0c9958183809188184ef7bb1573982175
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-02-04 11:18:53 +0100

    create private-policy

---
+++ src/pages/privacy-policy.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <main class="p-[5%] pt-20 sm:pt-24 max-w-[1500px] mx-auto">
+        <h1 class="my-2 mb-5 font-bold text-lg leading-tight">Politique de confidentialité</h1>
+        <p>Bienvenue sur NoHate, une plateforme dédiée à la sensibilisation et à la lutte contre le
+            cyberharcèlement. La protection de votre vie privée et de vos données personnelles est une priorité pour
+            nous. Cette politique de confidentialité explique quelles informations nous collectons, comment nous les
+            utilisons et quelles sont vos droits en matière de protection des données.</p>
+        <section class="mt-10">
+            <h2 class="font-bold text-base md:text-lg mb-3">1. Données collectées</h2>
+            <div>
+                <h3 class="font-medium text-sm md:text-base mt-3">1.1 Témoignages anonymes</h3>
+                <p>Lorsque vous soumettez un témoignage sur notre site, nous recueillons uniquement les informations que
+                    vous fournissez volontairement, telles que :
+                </p>
+                <ul>
+                    <li>Le type d'expérience (victime ou témoin)</li>
+                    <li>Le contenu de votre témoignage</li>
+                    <li>Aucune donnée permettant de vous identifier (nom, adresse e-mail, etc.) n'est demandée.</li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="font-medium text-sm md:text-base mt-3">1.2 Données de navigation et cookies</h3>
+                <p>Nous utilisons des outils analytiques pour collecter des informations anonymes sur l'utilisation de
+                    notre site, notamment :</p>
+                <ul>
+                    <li>Pages visitées</li>
+                    <li>Temps passé sur le site</li>
+                    <li>Type d'appareil utilisé</li>
+                    <li>Adresse IP anonymisé</li>
+                </ul>
+                <p>Cela dans le but d'améliorer l'expérience utilisateur et optimiser le contenu du site.</p>
+            </div>
+            <div>
+                <h3 class="font-medium text-sm md:text-base mt-3">1.3 Ressources et contenus interactifs</h3>
+                <p>Nous proposons des ressources (articles, vidéos, guides, etc.) qui peuvent inclure des liens vers des
+                    sites tiers. Nous ne sommes pas responsables des politiques de confidentialité de ces sites
+                    externes.</p>
+            </div>
+        </section>
+        <section class="mt-10">
+            <h2 class="font-bold text-base md:text-lg mb-3">2. Partage des données</h2>
+            <p>Vos informations ne sont jamais vendues ni partagées à des tiers à des fins commerciales.</p>
+        </section>
+        <section class="mt-10 sm:mt-10">
+            <h2 class="font-bold text-base md:text-lg mb-3">3. Vos droits</h2>
+            <p>Vous disposez des droits suivants concernant vos données personnelles :</p>
+            <ul>
+                <li>Droit d'accès : Vous pouvez demander quelles données sont collectées.</li>
+                <li>Droit de suppression : Vous pouvez demander la suppression de votre témoignage si vous en faites la
+                    demande à <a href="@mailto:contact@nohate.com" class="underline">contact@nohate.com</a>.</li>
+                <li>Droit d'opposition : Vous pouvez refuser l'utilisation des cookies analytiques via les
+                    paramètres de votre navigateur.</li>
+            </ul>
+        </section>
+        <section class="mt-10">
+            <h2 class="font-bold text-base md:text-lg mb-3">4. Contact</h2>
+            <p>Si vous avez des questions ou souhaitez exercer vos droits, vous pouvez nous contacter à <a
+                    href="@mailto:contact@nohate.com" class="underline">contact@nohate.com</a>.</p>
+        </section>
+    </main>
+</template>
