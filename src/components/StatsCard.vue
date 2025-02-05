416c19269eb182d24c5ae6c0176ca4c396233496
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-02-05 13:52:00 +0100

    update card

--- src/components/StatsCard.vue
+++ src/components/StatsCard.vue
@@ -8,12 +8,9 @@ defineProps<{
 </script>
 
 <template>
-    <a :href="link" target="_blank">
-        <div
-            class="p-5 max-sm:border-r max-sm:rounded-t-xl border-l border-t rounded-tl-xl leading-tight max-w-[450px]">
-            <div class="text-[30px] md:text-2xl mb-2 font-semibold">{{ stats }}</div>
-            <p>{{ description }}</p>
-            <p class="text-[14px] mt-2 font-light">({{ source }})</p>
-        </div>
-    </a>
+    <div class="p-5 max-sm:border-r max-sm:rounded-t-xl border-l border-t rounded-tl-xl leading-tight max-w-[450px]">
+        <div class="text-[30px] md:text-2xl mb-2 font-semibold">{{ stats }}</div>
+        <p>{{ description }}</p>
+        <div class="text-[14px] mt-2 font-light"><a :href="link">(Source : {{ source }})</a></div>
+    </div>
 </template>
