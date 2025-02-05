4202c777d6544229ebaae7e714462f5f746db034
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-02-05 13:41:20 +0100

    creation component : card for stats

---
+++ src/components/StatsCard.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+    stats: string;
+    description: string;
+    source?: string;
+    link?: string
+}>()
+</script>
+
+<template>
+    <a :href="link" target="_blank">
+        <div
+            class="p-5 max-sm:border-r max-sm:rounded-t-xl border-l border-t rounded-tl-xl leading-tight max-w-[450px]">
+            <div class="text-[30px] md:text-2xl mb-2 font-semibold">{{ stats }}</div>
+            <p>{{ description }}</p>
+            <p class="text-[14px] mt-2 font-light">({{ source }})</p>
+        </div>
+    </a>
+</template>
