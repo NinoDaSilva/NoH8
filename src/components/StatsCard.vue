e2aa570f9fda0126d1f8da0a8f0a9982825897da
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2025-02-09 20:23:50 +0100

    fix err : condition  for "source" paragraph

--- src/components/StatsCard.vue
+++ src/components/StatsCard.vue
@@ -11,6 +11,6 @@ defineProps<{
     <div class="p-5 max-sm:border-r max-sm:rounded-t-xl border-l border-t rounded-tl-xl leading-tight max-w-[450px]">
         <div class="text-[30px] md:text-2xl mb-2 font-semibold">{{ stats }}</div>
         <p>{{ description }}</p>
-        <div class="text-[14px] mt-2 font-light"><a :href="link">(Source : {{ source }})</a></div>
+        <div v-if="source" class="text-[14px] mt-2 font-light"><a :href="link">(Source : {{ source }})</a></div>
     </div>
 </template>
